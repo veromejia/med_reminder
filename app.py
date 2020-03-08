8ca86f9565a1657042a484822f073243416161ec
veromejia <veromejia.q@gmail.com>
2020-03-07 23:35:14 -0800

    make it better

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from forms.prescriptionform import PrescriptionForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e045fd27f61b2a04b5edbb45fa1350045fe8eb10653be9631d422f6427ba77b6'
 
-@app.route('/',methods=['POST','GET'])
-@app.route('/home', methods=['POST','GET'])
+@app.route('/', methods=['POST', 'GET'])
+@app.route('/home', methods=['POST', 'GET'])
 def home():
     if request.method == 'POST':
         if request.form['submit'] == 'ADD PATIENT':
@@ -17,23 +17,19 @@ def home():
     
     return render_template("/welcome.html")
 
-@app.route('/recipient/',methods=['POST','GET'])
+@app.route('/recipient', methods=['POST', 'GET'])
 def recipient():
     form = PatientForm()
     if form.validate_on_submit():
         form.save()
         return redirect( url_for('home') )
-    return render_template('recipient.html',form=form)
+    return render_template('recipient.html', form=form)
 
 @app.route('/reminder', methods=['POST','GET'])
 def reminder():
     form = PrescriptionForm()
     form.getPatients()
-    print(form.printObj())
-    print(form.start_dt.data)
-    print(form.end_dt.data)
-    print(form.validate_on_submit())
-
+   
     if request.method == 'POST':
         if form.validateForm():
             form.save()
